6937e76ab27b1e6df183aefdd012017d6a8e7b4f
YH <yh0729@kakao.com>
2024-08-05 21:23:33 +0900

    첫번째 커밋
    RDA 프로젝트 생성

---
+++ src/test/java/com/example/rda/RdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
